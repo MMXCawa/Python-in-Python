2a63423c81c98aaa83e400a2495d749cd218f6a1
MMXCawa <128949975+MMXCawa@users.noreply.github.com>
2023-03-26 12:02:09 +0800

    Add files via upload
    !

---
+++ p_i_p_1_0_3.py
@@ -0,0 +1,28 @@
+print('''python in python 1.0.3 (v1.0.3\4, Mar 19 2023 15:57:35)
+Type "help", "copyright", "credits" or "license()" for more information.
+      ''')
+
+
+
+while True:
+    command=input('>>>')
+
+    if 'copyright'==command:
+        print('''Copyright (c) 2001-2022 Python Software Foundation.
+All Rights Reserved.
+
+Copyright (c) 2000 BeOpen.com.
+All Rights Reserved.
+
+Copyright (c) 1995-2001 Corporation for National Research Initiatives.
+All Rights Reserved.
+
+Copyright (c) 1991-1995 Stichting Mathematisch Centrum, Amsterdam.
+All Rights Reserved.''')
+
+    if 'cridits' == command:
+        print('''Thanks to CWI, CNRI, BeOpen.com, Zope Corporation and a cast of thousands
+for supporting Python development.  See www.python.org for more information.
+Python in Python by MMXCawa''')
+    if 'license()' == command:
+        license()
