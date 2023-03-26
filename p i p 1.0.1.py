eba6421bf50496bad510f7fc6ab12f07697b783e
MMXCawa <128949975+MMXCawa@users.noreply.github.com>
2023-03-26 11:44:55 +0800

    Add files via upload

---
+++ p i p 1.0.1.py
@@ -0,0 +1,18 @@
+print('''python in python 1.0.1 (v1.0.1\2, Mar 19 2023 15:57:35)
+      Type "help", "copyright", "credits" or "license()" for more information.
+      ''')
+while True:
+    command=input('>>>')
+    if 'copyright'==command:
+        print('''Copyright (c) 2001-2022 Python Software Foundation.
+All Rights Reserved.
+
+Copyright (c) 2000 BeOpen.com.
+All Rights Reserved.
+
+Copyright (c) 1995-2001 Corporation for National Research Initiatives.
+All Rights Reserved.
+
+Copyright (c) 1991-1995 Stichting Mathematisch Centrum, Amsterdam.
+All Rights Reserved.
+''')
